e7d8349b315ca7a31069716a5e00714cdc47892e
LeonFeldmann <Leon_Feldmann@t-online.de>
2019-10-12 15:20:32 +0200

    added login and database

--- app.js
+++ app.js
@@ -1,22 +1,102 @@
-const express = require('express');
-// const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
-const app =  express();
-const fs = require('fs');
-const path = require('path');
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
+var express     = require('express'),
+    bodyParser  = require('body-parser'),
+    fs          = require('fs'),
+    path        = require('path'),
+    passport    = require("passport"),
+    mongo        = require("mongodb").MongoClient,
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
+var mongoose      = require("mongoose");
+var User          = require("./models/user");
+
+var url = process.env.MONGODB_URI || "mongodb://localhost/data";
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+
+
+var app =  express();
+app.set('view engine', 'ejs');
+app.use(require("express-session")({
+  secret: "Anything at all",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: true }));
+const port = process.env.PORT || 3000;
+
+passport.use(new LocalStrategy(User.authenticate()));
+//reads and en/decodes the session
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+// ==================
+//       routes
+// ==================
+
+app.get('/', (req, res) => res.render('homepage'));
+
+// login/logout
+app.get("/login", function(req, res){
+res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/logged",
+  failureRedirect: "/login"
+}), function(req, res){
+
+});
+
+app.get("/logout", function(req, res){
+  req.logOut();
+  res.redirect("/");
+});
+
+
+//middelware: check login status, if not logged in redirect to /login
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
 }
 
-app.use(bodyParser.urlencoded({ extended: true }));
-require('./app/routes')(app, {});
 
 
 
-// routes
+// show signup form
+app.get('/register', function(req, res){
+  res.render("register");
+});
+// handling user sign up
+app.post("/register", function(req, res){
+  req.body.username
+  req.body.password
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/logged");
+    });
+  });
+});
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get("/logged", isLoggedIn, function(req, res){
+res.send("This is content for logged users");
+});
+
+
 
 app.get('/document', function(req, res, next) {
 
@@ -29,6 +109,20 @@ app.get('/document', function(req, res, next) {
   res.send();
 });
 
+app.get('/document/:docId'), function(req, res) {
+
+  // filter db for doc with docId
+  // -> if found send doc
+  // -> if not found send empty obj?
+
+  res.send("This route is not implemented yet");
+}
+
+
+
+
+
+
 
 // catch 400
 app.use((err, req, res, next) => {
@@ -39,6 +133,8 @@ app.use((err, req, res, next) => {
 
 
 
-
-app.listen(port, () => {console.log('Example app listening in port: ' + port);});
+require('./app/routes')(app, {});
+app.listen(port, () => {
+  console.log('The app listening on port: ' + port);
+});
 
